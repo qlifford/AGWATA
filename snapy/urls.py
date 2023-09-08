12a863149735e261f839a697ce05c6657e54f074
qlifford <otienocee@gmail.comgit config --global user.nameqlofford>
2023-09-08 23:23:15 +0300

    2

--- snapy/urls.py
+++ snapy/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.Snapy.as_view(), name='snapy'),
     path('gallery/<int:pk>', views.Gallery.as_view(), name='gallery'),
     path('add/', views.Add.as_view(), name='add'),
-   
 ]
