57e5ff24b188db998d8d722cfd8e52a08d289dbf
qlifford <otienocee@gmail.comgit config --global user.nameqlofford>
2023-09-08 23:22:32 +0300

    commit

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/', include('shop.urls')),
-    path('money/', include('moneypath.urls')),
-    path('photos/', include('snapy.urls')),
+    path('kwach/shop', include('shop.urls')),
+    path('kwach/money/', include('moneypath.urls')),
+    path('kwach/photos/', include('snapy.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- snapy/urls.py
+++ snapy/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Snapy.as_view(), name='snapy'),
-    path('snapy/<str:pk>/', views.Gallery.as_view(), name='snapy'),
+    path('gallery/<int:pk>', views.Gallery.as_view(), name='gallery'),
     path('add/', views.Add.as_view(), name='add'),
    
 ]
